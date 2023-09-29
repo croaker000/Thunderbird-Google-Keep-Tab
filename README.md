e5f6adb78b916f675c5ad315486102a6ed09fa96
Martin Gautier <martin.gautier@myrnham.co.uk>
2023-09-29 12:50:56 +0100

    Modernised fore TB >= 115.* Added Spaces Toolbar Support

--- README.md
+++ README.md
@@ -1,15 +1,8 @@
 # Google Keep Tab
-Unofficial Google Keep add-on for Thunderbird, it adds a button that opens a Google Keep tab in Thunderbird.
-The [home page](https://addons.thunderbird.net/thunderbird/addon/google-keep-tab) of the extension contains some pictures and reviews.
+Fear of Mice's Google Keep add-on for Thunderbird, it adds a button that opens a Google Keep tab in Thunderbird.
 
-#### Installing 
-Open Thunderbird, go to Tools -> Add-ons -> Extensions, search for Google Keep in the serach box and click on "+ Add to Thunderbird".
+#### Installing
+Open Thunderbird, go to Tools -> Add-ons -> Extensions, search for Google Keep in the search box and click on "+ Add to Thunderbird".
 
 #### Installing from sources
-Download the repository, zip it, rename it to Google-Keep-Tab.xpi and choose install addon from file in Thunderbird.
-
-In linux the xpi file can be created with the following commands
-* `git clone https://github.com/Garoe/Thunderbird-Google-Keep-Tab`
-* `cd ./Thunderbird-Google-Keep-Tab`
-* `VERSION=$(cat ./manifest.json | jq --raw-output '.version')`
-* `zip -r "../Google-Keep-Tab-${VERSION}-tb.xpi" *`
+Download the repository, zip it, rename it to Google-Keep.xpi and choose install addon from file in Thunderbird.
--- src/background.js
+++ src/background.js
@@ -1,4 +1,10 @@
 const is_release = true;
+var browser = browser;
+var myTitle = "Google Keep";
+var myurl = "https://keep.google.com";
+var iconPath = "skin/google_keep_icon.svg";
+var spacesToolbarButtonId = "fom_gk_" + (Math.floor(Math.random() * 99999999)).toString();
+var isSpacesToolbarButtonOn = false;
 
 function debug_msg(message){
   if(is_release){
@@ -7,21 +13,103 @@ function debug_msg(message){
   console.debug("Google-Keep-Tab: " + message);
 }
 
-debug_msg("Loading");
+function initialize() {
+   debug_msg("Loading");
+   createContextMenu();
+   setSpacesToolbarButton();
+
+   if ("browserAction" in browser) {
+     debug_msg("Has permision for browserAction");
+
+     browser.browserAction.onClicked.addListener(async () => {
+       openNewTab(myurl);
+     });
+   } else {
+     openNewTab(myurl);
+   }
+
+   debug_msg("Loaded");
+}
+
+function getButtonProperties() {
+    return {
+        defaultIcons: {
+            16: iconPath,
+            32: iconPath
+        },
+        title: myTitle,
+        url: myurl
+    };
+}
+
+function createContextMenu() {
+    browser.menus.create({
+        id: "contextMenuEntry",
+        title: myTitle,
+        type: "normal",
+        contexts: ["browser_action"],
+        onclick: openNewTab
+    }, console.log("Google Keep Web context menu created."));
+}
+
+function setSpacesToolbarButton() {
+    var getWaInThMode = browser.storage.local.get("fom-gk-spaces-toolbar");
+    getWaInThMode.then(function (storedValue) {
+        if (storedValue["fom-gk-spaces-toolbar"] === "true") {
+            if (isSpacesToolbarButtonOn) {
+                refreshSpacesToolbar();
+            }
+            else {
+                addSpacesToolbarButton();
+            }
+            return;
+        }
+        if (storedValue["fom-gk-spaces-toolbar"] === "false") {
+            if (isSpacesToolbarButtonOn) {
+                removeSpacesToolbarButton();
+            }
+            return;
+        }
+        browser.storage.local.set({"fom-gk-spaces-toolbar":"true"});
+        addSpacesToolbarButton();
+    });
+}
+
+function addSpacesToolbarButton() {
+    try {
+        browser.spacesToolbar.addButton(spacesToolbarButtonId, getButtonProperties());
+        isSpacesToolbarButtonOn = true;
+        console.log("Google Keep Web spaces toolbar menu created.");
+    }
+    catch (e) {
+        console.log("spacesToolbar is not defined...\n", e);
+    }
+}
+function removeSpacesToolbarButton() {
+    try {
+        browser.spacesToolbar.removeButton(spacesToolbarButtonId);
+        isSpacesToolbarButtonOn = false;
+        console.log("Google Keep Web spaces toolbar menu removed.");
+    }
+    catch (e) {
+        console.log("spacesToolbar is not defined...\n", e);
+    }
+}
+function refreshSpacesToolbar() {
+    try {
+        if (isSpacesToolbarButtonOn) {
+            browser.spacesToolbar.updateButton(spacesToolbarButtonId, getButtonProperties());
+        }
+        console.log("Google Keep Web spaces toolbar menu refreshed.");
+    }
+    catch (e) {
+        console.log("spacesToolbar is not defined...\n", e);
+    }
+}
 
 function openNewTab(tab_url){
   debug_msg(`Opening tab with url: ${tab_url}`);
   browser.tabs.create({ url: tab_url });
 }
 
-if ("browserAction" in browser) {
-  debug_msg("Has permision for browserAction");
-
-  browser.browserAction.onClicked.addListener(async () => {
-    openNewTab("https://keep.google.com");
-  });
-} else {
-  openNewTab("https://keep.google.com");
-}
-
-debug_msg("Loaded");
+initialize();
